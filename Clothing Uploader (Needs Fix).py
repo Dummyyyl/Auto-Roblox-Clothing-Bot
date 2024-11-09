b6223254ab37067d6c0aa3d858568f9b8bc995e3
NotDummy <119734972+Dummyyyl@users.noreply.github.com>
2024-11-09 19:14:43 +0100

    Update Clothing Uploader (Needs Fix).py

--- Clothing Uploader (Needs Fix).py
+++ Clothing Uploader (Needs Fix).py
@@ -1,43 +1,44 @@
+import json
+import os
+import time
+from cleantext import clean
+import requests
+from colorama import init, Fore, Back
+
+# Initialize colorama for colored output in console
+init()
+
+# Load config.json instead of Config.ini
 try:
-    import time
-    import colorama
-    import requests
-    import time
-    import re
-    import configparser
-    from cleantext import clean
-    config = configparser.ConfigParser()
-    config.read_file(open(r"Config.ini"))
-    import os
-    from colorama import init, Fore, Back, Style
-    init()
-# If user doesn't have one of the modules, it will tell them to install all of the required
-except ImportError:
-    print("[ERROR] Failed to import some modules, make sure to run requirements.bat, delete all other python versions and install python 3.10.0 installed with add to path option checked during installation")
+    with open("config.json", "r") as file:
+        config = json.load(file)
+except FileNotFoundError:
+    print("[ERROR] Configuration file 'config.json' not found. Please ensure it's in the same directory.")
     input()
 
-cookie = str(config.get("auth","cookie"))
-group = str(config.get("clothing","group"))
-description = str(config.get("clothing","description"))
-priceconfig = int(config.get("clothing","price"))
-ratelimz = int(config.get("optional","ratelimitwaitseconds"))
-maxrobux = int(config.get("optional","maxrobuxtospend"))
-debugmode = config.getboolean('optional', 'debugmode') 
+# Extract variables from config.json
+cookie = str(config["auth"]["cookie"])
+group = str(config["clothing"]["group"])
+description = str(config["clothing"]["description"])
+priceconfig = int(config["clothing"]["price"])
+ratelimz = int(config["optional"]["ratelimitwaitseconds"])
+maxrobux = int(config["optional"]["maxrobuxtospend"])
+debugmode = config["optional"].get("debugmode", False)
 
-
+# Set up path and initialize session for Roblox authentication
 path = os.getcwd()
 try:
-    os.remove(f"{path}\\Storage\\Clothes\\Shirts\\deleteme.png")
-    os.remove(f"{path}\\Storage\\Clothes\\Pants\\deleteme.png")
-except:
+    os.remove(f"{path}/Storage/Clothes/Shirts/deleteme.png")
+except FileNotFoundError:
     pass
 
-# Authentication into roblox account
+# Authentication into Roblox account
 session = requests.Session()
 session.cookies[".ROBLOSECURITY"] = cookie
 req = session.post(url="https://auth.roblox.com/")
-if "X-CSRF-Token" in req.headers: 
-    session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]  
+if "X-CSRF-Token" in req.headers:
+    session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]
+
 req2 = session.post(url="https://auth.roblox.com/")
 try:
     getuser = session.get("https://users.roblox.com/v1/users/authenticated")
@@ -46,23 +47,21 @@ try:
     getuser4 = getuser2['name']
     print(f"{Back.CYAN}{Fore.BLACK}[Authentication]{Back.BLACK}{Fore.WHITE} Logged in as {getuser4}")
 except:
-    print(f"{Back.RED}{Fore.BLACK}[Error]{Back.BLACK}{Fore.WHITE} Your cookie is invalid")
-    print(f"{Back.YELLOW}{Fore.BLACK}[Info]{Back.BLACK}{Fore.WHITE} Please restart the program, with a valid cookie")
+    print(f"{Back.RED}{Fore.BLACK}[Error]{Back.BLACK}{Fore.WHITE} Invalid cookie")
+    print(f"{Back.YELLOW}{Fore.BLACK}[Info]{Back.BLACK}{Fore.WHITE} Please restart the program with a valid cookie")
     input()
 
-# Main program
+# Main program setup
 brokie = session.get("https://economy.roblox.com/v1/user/currency")
-brokie = brokie.json()
-brokie=brokie["robux"]
-print(f"{Back.CYAN}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} Remaining: R$ {brokie}")
-print("\n")
-pants = False
+brokie = brokie.json()["robux"]
+print(f"{Back.CYAN}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} Remaining: R$ {brokie}\n")
+
 assetid = "1"
 robuxspent = 0
 
-
+# Define the function for uploading shirts
 def shirts():
-    global group,description,priceconfig, name,creator,creatortype, pants,assetid, robuxspent, maxrobux
+    global group, description, priceconfig, assetid, robuxspent, maxrobux
 
     if robuxspent >= maxrobux:
         print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Max robux spent reached, stopping program")
@@ -71,132 +70,83 @@ def shirts():
 
     try:
         path = os.getcwd()
-        if pants == False:
-            pathz = f"{path}\\Storage\\Clothes\\Shirts"
-        else:
-            pathz = f"{path}\\Storage\\Clothes\\Pants"
-        name = os.listdir(pathz)[0]
-        name = name.split(".")
-        name = name[0]
+        pathz = f"{path}/Storage/Clothes/Shirts"
+        name = os.listdir(pathz)[0].split(".")[0]
+        name = clean(name, no_emoji=True)
         creator = group
         creatortype = "Group"
-        name = clean(name, no_emoji=True)
-    except:
-        if pants == False:
-            
-            print(f"{Back.MAGENTA}{Fore.BLACK}[Shirts]{Back.BLACK}{Fore.WHITE} All shirts have been uploaded, moving to pants\n")
-            pants = True
-            shirts()
-            return
-            
-        else:
-            print(f"{Back.MAGENTA}{Fore.BLACK}[Pants]{Back.BLACK}{Fore.WHITE} All pants have been uploaded, you may close the program")
-            input()
-            return
-        return "hey"
-        pants = True
-        shirts()
-        
-    json = open("Storage\Json\config.json","w")
-    json.write(f"""{{"name":"{name}","description":"{description}","creatorTargetId":"{creator}","creatorType":"{creatortype}"}}""")
-    json.close()
+    except IndexError:
+        print(f"{Back.MAGENTA}{Fore.BLACK}[Shirts]{Back.BLACK}{Fore.WHITE} All shirts uploaded. You may close the program.")
+        input()
+        return
 
-    path = os.getcwd()
-    if pants == False:
-        
-        link = "https://itemconfiguration.roblox.com/v1/avatar-assets/11/upload"
-    if pants == True:
-        link = "https://itemconfiguration.roblox.com/v1/avatar-assets/12/upload"
-    
+    # Save config data to JSON format in file
+    with open("Storage/Json/config.json", "w") as json_file:
+        json.dump({
+            "name": name,
+            "description": description,
+            "creatorTargetId": creator,
+            "creatorType": creatortype
+        }, json_file)
+
+    # Set link for shirt upload
+    link = "https://itemconfiguration.roblox.com/v1/avatar-assets/11/upload"
     files = {
-        'media': open(fr"{pathz}\\{os.listdir(pathz)[0]}", 'rb'),
-        'config': open('Storage\Json\config.json', 'rb')
-        }
-    s = session.post(link,files=files)
-    if debugmode == True:
+        'media': open(f"{pathz}/{os.listdir(pathz)[0]}", 'rb'),
+        'config': open('Storage/Json/config.json', 'rb')
+    }
+    s = session.post(link, files=files)
+    if debugmode:
         print(f"Status: {s.status_code}\nResponse: {s.text}")
     files["media"].close()
 
     sd = s.json()
-   
     try:
         assetid = sd['assetId']
-    except:
-        #files["media"].close()
-        #os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-        #print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid template: {name}\n")
-        code = sd['errors'][0]['code']
-        if code == 16:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Clothing name not allowed or invalid description, removing from list: {name}")
-            files["media"].close()
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-            shirts()
-            return
-        elif code == 11:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Clothing name not allowed or invalid description, removing from list: {name}")
-            files["media"].close()
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-            shirts()
-            return
-        elif code == 8:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid file type extension, removing from list: {name}")
-            files["media"].close()
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-            shirts()
-            return
-        elif code == 0:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Ratelimited, failed to upload (waiting {ratelimz}s): {name}\n")
-            time.sleep(ratelimz)
-            shirts()
-            return
-        elif code == 6:
-            print(f"{Back.RED}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} You don't have 10 robux to upload: {name}")
-            input()
-        elif code == 7:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid template, removing from list: {name}")
-            files["media"].close()
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-            shirts()
-            return
-        elif code == 9:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} You do not have permission to upload to the group")
-            shirts()
-            return
-            
+    except KeyError:
+        error_handling(sd, pathz)
+        return
 
-    pricefiles = {"price":priceconfig,"priceConfiguration":{"priceInRobux":priceconfig},"saleStatus":"OnSale"}
+    set_price(assetid)
+
+    brokie = session.get("https://economy.roblox.com/v1/user/currency").json()["robux"]
+    print(f"{Back.CYAN}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} Remaining: R$ {brokie}\n")
+    shirts()
+
+def error_handling(sd, pathz):
+    code = sd['errors'][0]['code']
+    name = os.listdir(pathz)[0].split(".")[0]
+
+    if code == 16 or code == 11:
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid name/description, removing: {name}")
+    elif code == 8:
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid file type, removing: {name}")
+    elif code == 0:
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Ratelimited. Retrying in {ratelimz}s: {name}")
+        time.sleep(ratelimz)
+    elif code == 6:
+        print(f"{Back.RED}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} Insufficient robux to upload: {name}")
+        input()
+    elif code == 7:
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Invalid template, removing: {name}")
+    elif code == 9:
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Permission denied for group upload.")
+    
+    os.remove(f"{pathz}/{name}.png")
+    shirts()
+
+def set_price(assetid):
+    pricefiles = {
+        "price": priceconfig,
+        "priceConfiguration": {"priceInRobux": priceconfig},
+        "saleStatus": "OnSale"
+    }
     priceupdate = f"https://itemconfiguration.roblox.com/v1/assets/{assetid}/release"
-    price = session.post(priceupdate,json=pricefiles)
-
-    if pants == False:
-        if s.status_code == 200:
-            print(f"{Back.GREEN}{Fore.BLACK}[Upload]{Back.BLACK}{Fore.WHITE} Successfully uploaded a shirt: {name}")
-            robuxspent+=10
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-        else:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Failed to upload a shirt: {name}")
-    else:
-        if s.status_code == 200:
-            print(f"{Back.GREEN}{Fore.BLACK}[Upload]{Back.BLACK}{Fore.WHITE} Successfully uploaded pants: {name}")
-            robuxspent+=10
-            os.remove(fr"{pathz}\\{os.listdir(pathz)[0]}")
-        else:
-            print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Failed to upload pants: {name}")
+    price = session.post(priceupdate, json=pricefiles)
 
     if price.status_code == 200:
         print(f"{Back.GREEN}{Fore.BLACK}[Upload]{Back.BLACK}{Fore.WHITE} Successfully set price to R$ {priceconfig}")
     else:
-        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Failed to set a price: {name}")
-    print("\n")
-    brokie = session.get("https://economy.roblox.com/v1/user/currency")
-    brokie = brokie.json()
-    brokie=brokie["robux"]
-    print(f"{Back.CYAN}{Fore.BLACK}[Robux]{Back.BLACK}{Fore.WHITE} Remaining: R$ {brokie}")
-    print("\n")
+        print(f"{Back.RED}{Fore.BLACK}[Fail]{Back.BLACK}{Fore.WHITE} Failed to set price.")
 
-    shirts()
-if pants == False:
-    a = shirts()
-    if a == "hey":
-        pants = True
-        shirts()
+shirts()
