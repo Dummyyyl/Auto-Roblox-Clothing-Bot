9d2f7b0153817f3a3dfe4a8bd9658e72051353d9
Markus Eisenmann <119734972+Dummyyyl@users.noreply.github.com>
2024-11-14 21:44:26 +0100

    Update UploaderTest.py

--- UploaderTest.py
+++ UploaderTest.py
@@ -5,6 +5,13 @@ import numpy as np
 import time
 import pyperclip
 from pathlib import Path
+import json
+
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+description = config["clothing"]["description"]
 
 # Function to open the link in the default web browser
 def open_uploadlink(link):
@@ -50,52 +57,100 @@ url = f"https://create.roblox.com/dashboard/creations/upload?assetType=Shirt&gro
 open_uploadlink(url)
 
 # Path to the target image you want to detect on the screen
-image_path1 = r"Storage\OpenCVPic\upload_button_cv.png"
-image_path2 = r"Storage\OpenCVPic\navigate_folder.png"
-image_path3 = r"Storage\OpenCVPic\picture3.png"
-image_path4 = r"Storage\OpenCVPic\target_image.png"
+upload_button_path = "Storage\OpenCVPic\\upload_button_cv.png"
+navigate_path1 = "Storage\OpenCVPic\\navigate_folder1.png"
+navigate_path2 = "Storage\OpenCVPic\\navigate_folder_2.png"
+name_field_path = "Storage\OpenCVPic\\name_field.png"
+discription_field_path = "Storage\OpenCVPic\\description_field.png"
+upload_to_roblox = "Storage\OpenCVPic\\upload_to_roblox.png"
+
 
 # Now, continuously check for the target image on the screen
 while True:
-    position1 = find_image_on_screen(image_path1)
+    position1 = find_image_on_screen(upload_button_path)
+    # Move to Upload Button
     if position1:
         move_mouse_to_target(position1)
         pyautogui.click()
         time.sleep(2)
         
-        # Now search for position2 and interact with it
-        position2 = find_image_on_screen(image_path2)
+        # Move to the Path in the Explorer
+        position2 = find_image_on_screen(navigate_path1)
         move_mouse_to_target(position2)
+        position3 = find_image_on_screen(navigate_path2)
+        move_mouse_to_target(position3)
+
+        # Copy the Path
         pyautogui.click()
         time.sleep(0.1)
         pyautogui.hotkey('ctrl', 'c')
         time.sleep(0.1)
 
-        # Get the path from clipboard (e.g. "C:\Users\MarkusEisenmann\Documents\Code\Auto-Roblox-Clothing-Bot-main\Auto-Roblox-Clothing-Bot-main\Storage")
-        current_path = pyperclip.paste()
-
-        # Define the base and fixed path
+        # Edit Path
+        current_path = pyperclip.paste() # Set Current Path to clipboard
         base_path = r"Auto-Roblox-Clothing-Bot-main\Auto-Roblox-Clothing-Bot-main\Storage"
-        fixed_path = r"Clothes\Shirts"
+        fixed_path = r"\Clothes\Shirts"
 
-        # Check if the current path already includes the base_path
+        # Paste Path
         if base_path in current_path:
-            # If it already contains the base path, append only the fixed_path
+
             final_path = Path(current_path) / fixed_path
+            pyautogui.press('esc')
+
+
+            time.sleep(1) ##################### DELETE THIS
+            pyautogui.hotkey('Alt', 'f4') ##################### DELETE THIS
+
+
         else:
-            # If the base_path is not present, append the entire fixed path
             final_path = Path(current_path) / base_path / fixed_path
+            print(2)
+            pyperclip.copy(str(final_path))
+            pyautogui.hotkey('ctrl', 'v')
+            time.sleep(0.1)
+            pyautogui.press('enter')
+            time.sleep(0.1)
 
-        # Copy the final path back to the clipboard
-        pyperclip.copy(str(final_path))
 
-        time.sleep(0.1)
+            time.sleep(1) ##################### DELETE THIS
+            pyautogui.hotkey('Alt', 'f4') ##################### DELETE THIS
 
-        # Paste the copied path using 'ctrl + v'
-        pyautogui.hotkey('ctrl', 'v')
-        time.sleep(0.1)
-        pyautogui.press('enter')
+
+        # Move to the Target Image
         
+        # Move to the Name Field
+        position4 = find_image_on_screen(name_field_path)
+        move_mouse_to_target(position4)
+        time.sleep(1) ##################### EDIT DELAY
+
+        # Copy the Name
+        pyautogui.click()
+        pyautogui.click()
+        time.sleep(0.1)
+        pyautogui.hotkey('ctrl', 'c')
+
+        # Delete Picture with the Name
+
+        # Move to the Description Field
+        position5 = find_image_on_screen(discription_field_path)
+        move_mouse_to_target(position5)
+        time.sleep(0.5) ##################### EDIT DELAY
+            
+        # Paste the Description
+        pyperclip.copy(str(description))
+        pyautogui.click()
+        pyautogui.hotkey('ctrl', 'v')
+
+        # Scroll Down
+        time.sleep(0.2)
+        pyautogui.scroll(-500)
+
+        # Move to the Upload field and Upload It
+        position6 = find_image_on_screen(upload_to_roblox)
+        move_mouse_to_target(position6)
+        # pyautogui.click()
+
+
         break  # Exit the loop once the target is found and actions are performed
     
     time.sleep(1)  # Check every second
